3762c140e75bf1624f791f2b41e68de095fa2654
anzoman <aluzarwork@gmail.com>
2021-01-02 10:17:51 +0100

    Add health checks

--- api/CampingOverviewAPI/Startup.cs
+++ api/CampingOverviewAPI/Startup.cs
@@ -1,7 +1,9 @@
 using CampingOverviewAPI.Models;
 using CampingOverviewAPI.Services;
 using CampingOverviewAPI.Services.Interfaces;
+using HealthChecks.UI.Client;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
@@ -14,7 +16,6 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
-using System.Text;
 
 namespace CampingOverviewAPI
 {
@@ -40,13 +41,14 @@ namespace CampingOverviewAPI
             if (connectionString == null)
             {
                 services.AddEntityFrameworkNpgsql().AddDbContext<avtokampiContext>(options =>
-                    options.UseNpgsql(Configuration.GetConnectionString("Avtokampi"))
+                    options.UseNpgsql(Configuration.GetConnectionString("Avtokampi"), o => o.SetPostgresVersion(new Version(9, 6, 13)))
                 );
+                connectionString = Configuration.GetConnectionString("Avtokampi");
             }
             else
             {
                 services.AddEntityFrameworkNpgsql().AddDbContext<avtokampiContext>(options =>
-                    options.UseNpgsql(connectionString)
+                    options.UseNpgsql(connectionString, o => o.SetPostgresVersion(new Version(9, 6, 13)))
                 );
             }
 
@@ -71,6 +73,11 @@ namespace CampingOverviewAPI
                 var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
                 c.IncludeXmlComments(xmlPath);
             });
+
+            // health checks
+            services.AddHealthChecks().AddNpgSql(connectionString);
+
+            services.AddHealthChecksUI(setupSettings: setup => setup.AddHealthCheckEndpoint("Database connection", "/health")).AddInMemoryStorage();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -107,12 +114,15 @@ namespace CampingOverviewAPI
 
             app.UseRouting();
 
-            //app.UseAuthentication();
-            //app.UseAuthorization();
-
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                endpoints.MapHealthChecks("/health", new HealthCheckOptions
+                {
+                    Predicate = _ => true,
+                    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+                });
+                endpoints.MapHealthChecksUI();
             });
         }
     }
